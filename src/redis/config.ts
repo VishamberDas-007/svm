fa876ea1275d09ba28c7177342d30c72c5af6c85
Vishamber Das <vishdas1111@gmail.com>
2023-08-18 23:46:59 +0530

    fix(Redis): integration in progress

--- src/redis/config.ts
+++ src/redis/config.ts
@@ -1,19 +1,18 @@
-import { createClient } from 'redis'
+import redis from 'ioredis'
 import dotenv from 'dotenv'
 dotenv.config()
+const renderURL = process.env.REDIS_URL
 
-export const redisClient = (async () => {
-    const client = createClient({
-        url: process.env.REDIS_URL,
-    })
+const renderRedis = new redis(renderURL || '')
 
-    client.on('error', (err) => console.log('Redis Client Error', err))
+renderRedis.on('connect', () => {
+    console.log('Connected to Redis')
+})
 
-    await client.connect()
+export const setValueInRedis = async (obj: any) => {
+    await renderRedis.set('apple', 10)
+}
 
-    // Send and retrieve some values
-    await client.set('key', 'node redis')
-    const value = await client.get('key')
-
-    console.log('found value: ', value)
-})()
+export const getValueInRedis = async (key: string) => {
+    await renderRedis.get(key)
+}
