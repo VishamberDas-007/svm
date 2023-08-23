8a024326882ddd756f3fe04277a596b14856e53b
Vishamber Das <vishdas1111@gmail.com>
2023-08-23 20:43:43 +0530

    fix(Redis): make local redis configure and run successfully

--- src/redis/config.ts
+++ src/redis/config.ts
@@ -1,9 +1,9 @@
 import redis from 'ioredis'
 import dotenv from 'dotenv'
 dotenv.config()
-const renderURL = process.env.REDIS_URL
+// const renderURL = process.env.REDIS_URL
 
-const renderRedis = new redis(renderURL || '')
+const renderRedis = new redis()
 
 renderRedis.on('connect', () => {
     console.log('Connected to Redis')
@@ -16,3 +16,5 @@ export const setValueInRedis = async (obj: any) => {
 export const getValueInRedis = async (key: string) => {
     await renderRedis.get(key)
 }
+
+export { renderRedis }
