8342c4a8446167f998acacd44387c03d786c9a39
Vishamber Das <vishdas1111@gmail.com>
2023-08-29 23:09:41 +0530

    fix(Model): fix the model attribute

---
+++ src/db/migrations/20230829171026_update_installment_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Installment" ADD COLUMN     "penalty" INTEGER;
---
+++ src/db/migrations/20230829171454_remove_name_from_installment_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `Installment` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Installment" DROP COLUMN "name";
