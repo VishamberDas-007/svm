536eec3863c39628a0eda72e7886e9df5ba30db5
Vishamber Das <vishdas1111@gmail.com>
2023-09-03 23:30:57 +0530

    fix(Contact-Us): add filter in the list API

--- src/controllers/types/website.ts
+++ src/controllers/types/website.ts
@@ -1,7 +1,16 @@
+import { ContactUsStatus } from '@prisma/client'
+import { Request } from 'express'
+
 export type TContactUs = {
     name: string
     email?: string
     number: string
     subject: string
     message: string
+}
+
+export type TFetchContactUsListReq = Request & {
+    query: {
+        status: ContactUsStatus
+    }
 }
--- src/controllers/website.controller.ts
+++ src/controllers/website.controller.ts
@@ -2,8 +2,10 @@ import { Request, Response } from 'express'
 import prisma from '../db'
 import catchAsync from '../utils/catchAsync'
 import responseHandler from '../utils/responseHandler'
-import { TContactUs } from './types/website'
+import { TContactUs, TFetchContactUsListReq } from './types/website'
 import { WEBSITE_S_0001, WEBSITE_S_0002 } from '../config/responseCodes/website'
+
+// !!!!!! validation is pending !!!!!!!!!!!!!!
 
 export const saveContactUs = catchAsync(async (req: Request, res: Response) => {
     const { email, name, message, number, subject }: TContactUs = req.body
@@ -22,8 +24,26 @@ export const saveContactUs = catchAsync(async (req: Request, res: Response) => {
 })
 
 export const fetchContactUsList = catchAsync(
-    async (req: Request, res: Response) => {
-        const contactUsList = await prisma.contactUs.findMany()
+    async (req: TFetchContactUsListReq, res: Response) => {
+        const { status } = req.query
+        let whereClause = {}
+
+        if (status === 'COMPLETED') {
+            whereClause = {
+                status: 'COMPLETED',
+            }
+        } else if (status === 'PENDING') {
+            whereClause = {
+                status: 'PENDING',
+            }
+        }
+
+        const contactUsList = await prisma.contactUs.findMany({
+            where: whereClause,
+            orderBy: {
+                createdAt: 'desc',
+            },
+        })
 
         return responseHandler(res, WEBSITE_S_0002, contactUsList)
     }
