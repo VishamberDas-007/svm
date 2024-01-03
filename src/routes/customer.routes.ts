cb45a7198f060c7b66d9f6e5a955a2cd40bfc5c9
Vishamber Das <103981172+VishamberDas-007@users.noreply.github.com>
2024-01-03 08:54:15 +0530

    Main (#36)
    * refact(project): comment the middleware
    
    * fix(customer): fix the customer routes
    
    * fix the project images in progress
    
    * fix(Project): fix the project images
    
    * build(project): build the changes
    
    * ci-cd
    
    * ci-cd
    
    * ci-cd (#27)
    
    * ci-cd
    
    * ci-cd
    
    * feat(Project): add API to fetch images
    
    * build(project): build the code
    
    * Main (#28)
    
    * ci-cd
    
    * ci-cd
    
    * feat(Project): add API to fetch images
    
    * build(project): build the code
    
    * rebuild(project): rebuild after fix conflicts
    
    * fix(permission): fix the permission
    
    * build(project): build the project
    
    * fix(customer): make different APIs for image upload
    
    * build(customer): create build after the customer fixes
    
    * fix(customer): make changes in the customer image controller and fix the middleware sequence
    
    * build(customer): create build after the customer fixes
    
    * build(customer): create build after the customer fixes

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -13,27 +13,32 @@ customerRouter.post(
     //     { name: 'panImages', maxCount: 1 },
     //     { name: 'customerImage', maxCount: 3 },
     // ]),
+    // upload.fields([
+    //     { name: 'aadharImages', maxCount: 2 },
+    //     { name: 'panImages', maxCount: 1 },
+    //     { name: 'customerImage', maxCount: 3 },
+    // ]),
     customerController.newCustomer
 )
 
 customerRouter.patch(
     '/upload/pan-image/:customerId',
-    upload.single('panImages'),
     authMiddleware(['CUSTOMER_WRITE']),
+    upload.single('panImages'),
     customerController.uploadPanImage
 )
 
 customerRouter.patch(
     '/upload/aadhar-image/:customerId',
-    upload.fields([{ name: 'aadharImages', maxCount: 2 }]),
     authMiddleware(['CUSTOMER_WRITE']),
+    upload.fields([{ name: 'aadharImages', maxCount: 2 }]),
     customerController.uploadAadharImage
 )
 
 customerRouter.patch(
     '/upload/customer-image/:customerId',
-    upload.fields([{ name: 'customerImage', maxCount: 3 }]),
     authMiddleware(['CUSTOMER_WRITE']),
+    upload.fields([{ name: 'customerImage', maxCount: 3 }]),
     customerController.uploadCustomerImage
 )
 
